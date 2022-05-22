9dc8e0602073c7c1062f5180954c633f8b39d8e8
Fabricia Rodrigues <fabriciarodrigues45@gmail.com>
2022-05-22 15:56:29 -0300

    Lista 2

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [results, setResult] = useState([]);
+
+  const [key, setKey] = useState("");
+
+  const getSearchResult = (key) => {
+    fetch(`https://hn.algolia.com/api/v1/search?query=chave/${key}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setResult(data.hits);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <View style={styles.title}>
+        <h1>Pesquisa:</h1>
+      </View>
+
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder={"Digite o que deseja pesquisar..."}
+          value={key}
+          onChangeText={(searchKey) => setKey(searchKey)}
+        />
+        <Button title="Pesquisar" onPress={() => getSearchResult(search)} />
+      </View>
+
+      {results.map((result) => (
+        <View style={styles.result}>
+          <li>Autor: {result.author}</li>
+          <li>Título: {result.title}</li>
+          <li>URL: {result.url}</li>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  title: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  textInput: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    borderWidth: 3,
+    height: 35,
+  },
+
+  result: {
+    flex: 1,
+    marginBottom: 10,
+    width: "100%",
+    minHeight: 100,
+    borderWidth: 3,
+    justifyContent: "flex-end",
+    marginTop: 10,
   },
 });
