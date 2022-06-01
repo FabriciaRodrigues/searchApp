947a74655516c1643a299559347167a402cad316
Fabricia de Sousa Rodrigues <103256870+FabriciaRodrigues@users.noreply.github.com>
2022-05-31 22:16:35 -0300

    Prova 2

--- App.js
+++ App.js
@@ -1,76 +1,97 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import StarRating from "react-native-star-rating";
 
 export default function App() {
-  const [results, setResult] = useState([]);
+  const [book, setBook] = useState(null);
+  const [starts, setStars] = useState(Number);
+  const [search, setSearch] = useState(null);
 
-  const [key, setKey] = useState("");
+  const getBookData = (title) => {
+    const endpoint = `https://hn.algolia.com/api/v1/search?query=${title}/`;
 
-  const getSearchResult = (key) => {
-    fetch(`https://hn.algolia.com/api/v1/search?query=chave/${key}`)
-      .then((response) => response.json())
+    fetch(endpoint)
+      .then((resposta) => resposta.json())
       .then((data) => {
-        setResult(data.hits);
+        const products = data.hits;
+        setBook(products);
       });
   };
 
+  onStarRatingPress = (rating) => {
+    setStars(rating);
+  };
+
   return (
-    <View>
-      <View style={styles.title}>
-        <h1>Pesquisa:</h1>
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.topTitle}>Livraria</Text>
       </View>
-
-      <View style={styles.container}>
+      <ScrollView>
         <TextInput
-          style={styles.textInput}
-          placeholder={"Digite o que deseja pesquisar..."}
-          value={key}
-          onChangeText={(searchKey) => setKey(searchKey)}
+          style={styles.input}
+          placeholder="Pesquisar por titulo"
+          onChangeText={(newText) => setSearch(newText)}
+          defaultValue={search}
         />
-        <Button title="Pesquisar" onPress={() => getSearchResult(search)} />
-      </View>
-
-      {results.map((result) => (
-        <View style={styles.result}>
-          <li>Autor: {result.author}</li>
-          <li>Título: {result.title}</li>
-          <li>URL: {result.url}</li>
-        </View>
-      ))}
-      <StatusBar style="auto" />
+        <Button title="Pesquisar" onPress={() => getBookData(search)} />
+        {book?.map((b) =>
+          starts?.map((s) => (
+            <View style={styles.cardContainer}>
+              <StarRating
+                disabled={false}
+                maxStars={5}
+                rating={s.value}
+                selectedStar={(rating) => onStarRatingPress(rating)}
+              />
+              <Text style={styles.text}>Titulo: {b.title}</Text>
+              <Text style={styles.text}>Autor: {b.author}</Text>
+              <Text style={styles.text}>url: {b.url}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  title: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: { flex: 1, backgroundColor: "#fff" },
 
-  container: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-
-  textInput: {
-    justifyContent: "center",
-    alignItems: "center",
+  input: {
+    borderWidth: 2,
+    borderColor: "#000",
+    height: 40,
     textAlign: "center",
-    borderWidth: 3,
-    height: 35,
+    marginBottom: 12,
   },
 
-  result: {
-    flex: 1,
-    marginBottom: 10,
-    width: "100%",
-    minHeight: 100,
-    borderWidth: 3,
-    justifyContent: "flex-end",
-    marginTop: 10,
+  top: {
+    padding: 20,
+    paddingTop: 40,
+    marginBottom: 20,
+    backgroundColor: "#5671A8",
   },
+  topTitle: {
+    fontSize: 22,
+    marginBottom: 10,
+    color: "#fff",
+    textAlign: "center",
+  },
+
+  cardContainer: {
+    backgroundColor: "#5671A8",
+    borderRadius: 4,
+    marginTop: 10,
+    marginHorizontal: 20,
+    padding: 10,
+  },
+  text: { fontSize: 16, color: "#fff" },
 });
